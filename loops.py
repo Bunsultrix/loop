e2828f5b100b999c4bb5da9a060d0c5706c79204
Nitish Sushant Khrolia <83476150+Bunsultrix@users.noreply.github.com>
2021-05-10 18:12:02 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,25 @@
+print("loops")
+#listmy =["nitish","sushant" ,"khrolia","be mechanical" ,"pussgrc", "collage"]
+#print(type(listmy))
+#for item in listmy:
+ #   print(item)
+#newlist = [["gold ", 1] ,["silver" ,2] ,["bronze", 3]]
+#dict = dict(newlist)
+#print(dict)
+#for item in dict:
+ #   print(item)
+
+#for item,whichplace in dict.items():
+ #   print(item,"position", whichplace)
+
+
+#{while loop}
+i= 0
+while i<45:
+#    print(i)                       #{this is we print whole number}
+#    print(i + 1)                 #{this is how we print natural numbers starting from 1 to infinity}
+
+#    print(i+1,end = " ")          # {this how we print numbers in stright line}
+
+
+    i=i+2                          #{this is how we add number or skip the number}
